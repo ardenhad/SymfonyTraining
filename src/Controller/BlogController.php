8db4863520dea2614c4596f09a293879ee319376
Arden Had <arden.haddeler@ozu.edu.tr>
2019-07-16 14:47:05 +0300

    (Ignore last 2 commits) Implementation of twigs, filters and custom twig extensions.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,30 +5,90 @@ namespace App\Controller;
 
 use App\Services\Greeting;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 
+/**
+ * @Route("/blog")
+ */
 class BlogController extends AbstractController
 {
-    /**
-     * @var Greeting
-     */
-    private $greeting;
 
-    public function __construct(Greeting $greeting)
+    /**
+     * @var \Twig_Environment
+     */
+    private $twig;
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(\Twig_Environment $twig, SessionInterface $session, RouterInterface $router)
     {
-        $this->greeting = $greeting;
+
+        $this->twig = $twig;
+        $this->session = $session;
+        $this->router = $router;
     }
 
 
     /**
      * @Route("/", name="blog_index")
      */
-
-    public function index(Request $request)
+    public function index()
     {
-        return $this->render("base.html.twig", ["message" => $this->greeting->greet($request->get("name"))]);
+        $html = $this->twig->render("blog/index.html.twig",
+            [
+                "posts" => $this->session->get("posts")
+            ]
+        );
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/add", name="blog_add")
+     */
+    public function add()
+    {
+        $posts = $this->session->get("posts");
+        $posts[uniqid()] = [
+            "title" => "A random title ".rand(1, 500),
+            "text" => "Some random text".rand(1, 500),
+            "date" => new \DateTime()
+        ];
+        $this->session->set("posts", $posts);
+
+        return new RedirectResponse($this->router->generate("blog_index"));
+    }
+
+    /**
+     * @Route("/show/{id}", name = "blog_show")
+     */
+    public function show($id)
+    {
+        $posts = $this->session->get("posts");
+        if (!$posts || !isset($posts[$id])) {
+            throw new NotFoundHttpException("Post not found");
+        }
+
+        $html = $this->twig->render(
+            "blog/post.html.twig", [
+                "id" => $id ,
+                "post" => $posts[$id]
+            ]
+        );
+
+        return new Response($html);
     }
 
 
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,7 +4,24 @@
 namespace App\Twig;
 
 
-class AppExtension
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
 {
+    public function getFilters()
+    {
+        //"price" is the name of this filter, it is being used for the price.
+        //Callable part is pointing the class/function respectively.
+        return [
+            new TwigFilter("price", [$this, "priceFilter"])
+        ];
+    }
+    //The part where the job gets done obviously.
+    public function priceFilter($number)
+    {
+        return "$".number_format($number, 2, ".", ",");
+    }
+
 
 }
