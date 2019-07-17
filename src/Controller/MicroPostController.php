f737986fe2b81221f924f4e2ecd4245deaf87f3e
Arden Had <arden.haddeler@ozu.edu.tr>
2019-07-17 15:28:37 +0300

    Creating a form and handling submission.

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -3,10 +3,16 @@
 
 namespace App\Controller;
 
-
+use App\Entity\MicroPost;
 use App\Repository\MicroPostRepository;
+use App\Form\MicroPostType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * @Route("micro-post")
@@ -15,11 +21,28 @@ class MicroPostController
 {
     private $twig;
     private $microPostRepository;
+    private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
 
-    public function __construct(\Twig_Environment $twig, MicroPostRepository $microPostRepository)
-    {
-        $this->twig=$twig;
+    public function __construct(
+        \Twig_Environment $twig,
+        MicroPostRepository $microPostRepository,
+        FormFactoryInterface $formFactory,
+        EntityManagerInterface $entityManager,
+        RouterInterface $router
+    ) {
+        $this->twig = $twig;
         $this->microPostRepository = $microPostRepository;
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+        $this->router = $router;
     }
 
     /**
@@ -30,6 +53,32 @@ class MicroPostController
         $html = $this->twig->render("micro-post/index.html.twig", [
             "posts" => $this->microPostRepository->findAll()
         ]);
+
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/add", name="micro_post_add")
+     */
+    public function add(Request $request)
+    {
+        $microPost = new MicroPost();
+        $microPost->setTime(new \DateTime());
+
+        $form = $this->formFactory->create(MicroPostType::class, $microPost);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->persist($microPost);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate("micro_post_index"));
+        }
+
+        $html = $this->twig->render(
+            "micro-post/add.html.twig",
+            ["form" => $form->createView()]
+        );
 
         return new Response($html);
     }
